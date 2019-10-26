4ce7a492c5fce089934e5f79b483f96de6e4de94
Isaias Jair de Oliveira Junior <jrissi2008@gmail.com>
2019-10-26 19:04:18 -0300

    Created Main View Controller

--- src/gui/ControllerMainView.java
+++ src/gui/ControllerMainView.java
@@ -1,5 +1,48 @@
 package gui;
 
-public class ControllerMainView {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import model.entities.Person;
+
+public class ControllerMainView implements Initializable {
+	
+	@FXML
+	private Button btClick;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCalc;
+	
+	@FXML
+	ComboBox<Person> comboBoxPerson;
+	
+	@FXML
+	private TextField txtHeight;
+	
+	@FXML
+	private TextField txtWeight;
+	
+	@FXML
+	private TextField txtBmi;
+	
+	@FXML
+	public void onBtClick() {
+		
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
+	
+	
+	
 }
